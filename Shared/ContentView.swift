feffbe97e9830c387f91b7509c51f7749713c62b
divyakumarprajapati <divyakumarprajapati1@gmail.com>
2022-08-31 00:35:11 +0530

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,65 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Divyakumar Prajapati on 24/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            Image("background")
+            VStack {
+                Spacer()
+                Image("logo")
+                Spacer()
+                HStack {
+                    Spacer()
+                    Image("card3")
+                    Spacer()
+                    Image("card4")
+                    Spacer()
+                }
+                Spacer()
+                Image("dealbutton")
+                Spacer()
+                HStack {
+                    Spacer()
+                    VStack{
+                        Text("Player")
+                            .font(.headline)
+                            .fontWeight(.heavy)
+                            .foregroundColor(Color.white)
+                            .padding(.bottom)
+                        Text("0")
+                            .font(.headline)
+                            .fontWeight(.heavy)
+                            .foregroundColor(Color.white)
+                    }
+                    Spacer()
+                    VStack{
+                        Text("CPU")
+                            .font(.headline)
+                            .fontWeight(.heavy)
+                            .foregroundColor(Color.white)
+                            .padding(.bottom)
+                        Text("0")
+                            .font(.headline)
+                            .fontWeight(.heavy)
+                            .foregroundColor(Color.white)
+                    }
+                    Spacer()
+                }
+                Spacer()
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/swift_project1App.swift
@@ -0,0 +1,17 @@
+//
+//  swift_project1App.swift
+//  Shared
+//
+//  Created by Divyakumar Prajapati on 24/08/22.
+//
+
+import SwiftUI
+
+@main
+struct swift_project1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
